e7547d4ec8cd13e053c4c953234225ccaf797bbc
Miguel Fonseca <150562+mcsf@users.noreply.github.com>
2024-12-07 13:39:37 +0000

    Day 7: Add multiprocessing

--- 07/run.py
+++ 07/run.py
@@ -3,6 +3,8 @@
 from sys import stdin
 from re import split
 from operator import add, mul
+from multiprocessing import Pool, freeze_support
+from functools import partial
 
 
 def concat(a, b):  # Faster than int(''.join(str(...
@@ -16,7 +18,8 @@ def concat(a, b):  # Faster than int(''.join(str(...
 OPERATORS = [mul, add, concat]
 
 
-def is_solvable(target, operands, base=2):
+def solve(line, base=2):
+    target, *operands = line
     for i in range(base ** (len(operands) - 1)):
         acc = operands[0]
         for opd in operands[1:]:
@@ -25,16 +28,13 @@ def is_solvable(target, operands, base=2):
                 break
             i //= base
         if acc == target:
-            return True
+            return target
+    return 0
 
 
-pt1 = pt2 = 0
-for line in stdin:
-    target, *operands = list(map(int, split(r':?\s', line.strip())))
-    if is_solvable(target, operands, base=2):
-        pt1 += target
-    if is_solvable(target, operands, base=3):
-        pt2 += target
-
-print(pt1)
-print(pt2)
+if __name__ == '__main__':
+    freeze_support()
+    lines = [list(map(int, split(r':?\s', line.strip()))) for line in stdin]
+    pool = Pool()
+    print(sum(pool.map(partial(solve, base=2), lines)))
+    print(sum(pool.map(partial(solve, base=3), lines)))
