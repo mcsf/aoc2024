94cda4d3fb1b6a76272d8241fd369a90c1db1974
Miguel Fonseca <150562+mcsf@users.noreply.github.com>
2024-12-08 12:07:26 +0000

    Day 6: Use multiprocessing (8x speed imprv on M1 Pro)

--- 06/run.py
+++ 06/run.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 from sys import stdin
+from multiprocessing import Pool, freeze_support
+from functools import partial
 
 # Build sets of Points and Obstacles
 P = set()  # type: set[complex]
@@ -14,50 +16,51 @@ for y, line in enumerate(stdin):
             O.add(p)
         P.add(p)
 
-
 # Vary (x, y) according to rotation r
 ROT = [1, 1j, -1, -1j]
 
-# PART 1
-#
+
 # Capture all visited positions
+def walk():
+    p = start  # initial position
+    r = 3      # initial rotation, facing up
+    V = set()  # set[complex]
+    while p in P:
+        V.add(p)
+        if (ahead := p + ROT[r]) in O:
+            r = (r + 1) % 4
+        else:
+            p = ahead
+    return V
 
-V = set()  # type: set[complex]
-p = start  # initial position
-r = 3      # initial rotation, facing up
 
-while p in P:
-    V.add(p)
-    if (ahead := p + ROT[r]) in O:
-        r = (r + 1) % 4
-    else:
-        p = ahead
-
-# PART 2
-#
 # Walk along visited points looking for loops by placing hypothetical obstacles
+def is_obstacle(start: complex,      # Pass globals explicitly
+                P: set[complex],     # due to
+                ROT: list[complex],  # multiprocessing
+                O: set[complex],     # constraints.
 
-count = 0
+                o: complex):         # 'o' is the test subject
 
-for maybe_obstacle in V:
     # This time, track visits with rotation too
     Vr = set()  # type: set[tuple[complex, int]]
     p = start
     r = 3
-
-    O.add(maybe_obstacle)
-
+    O = O | {o}  # Don't mutate O, make a copy # noqa: E741
     while p in P:
         if (p, r) in Vr:
-            count += 1
-            break
+            return True
         Vr.add((p, r))
         if (ahead := p + ROT[r]) in O:
             r = (r + 1) % 4
         else:
             p = ahead
+    return False
 
-    O.remove(maybe_obstacle)
 
-print(len(V))
-print(count)
+if __name__ == '__main__':
+    freeze_support()
+    pool = Pool()
+    print(len(V := walk()))
+    task = partial(is_obstacle, start, P, ROT, O)
+    print(sum(1 for v in pool.map(task, V) if v))
