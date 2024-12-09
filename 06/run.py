fba1dd06c25155cfbe8caa2f6d6b6346a7148d96
Miguel Fonseca <150562+mcsf@users.noreply.github.com>
2024-12-09 14:47:40 +0000

    Day 6: Just use complex for the rotation, d'oh

--- 06/run.py
+++ 06/run.py
@@ -16,19 +16,16 @@ for y, line in enumerate(stdin):
             O.add(p)
         P.add(p)
 
-# Vary (x, y) according to rotation r
-ROT = [1, 1j, -1, -1j]
-
 
 # Capture all visited positions
 def walk():
     p = start  # initial position
-    r = 3      # initial rotation, facing up
+    r = -1j    # initial rotation
     V = set()  # set[complex]
     while p in P:
         V.add(p)
-        if (ahead := p + ROT[r]) in O:
-            r = (r + 1) % 4
+        if (ahead := p + r) in O:
+            r *= 1j
         else:
             p = ahead
     return V
@@ -36,23 +33,22 @@ def walk():
 
 # Walk along visited points looking for loops by placing hypothetical obstacles
 def is_obstacle(start: complex,      # Pass globals explicitly
-                P: set[complex],     # due to
-                ROT: list[complex],  # multiprocessing
+                P: set[complex],     # due to multiprocessing
                 O: set[complex],     # constraints.
 
                 o: complex):         # 'o' is the test subject
 
     # This time, track visits with rotation too
-    Vr = set()  # type: set[tuple[complex, int]]
+    Vr = set()  # type: set[tuple[complex, complex]]
     p = start
-    r = 3
+    r = -1j
     O = O | {o}  # Don't mutate O, make a copy # noqa: E741
     while p in P:
         if (p, r) in Vr:
             return True
         Vr.add((p, r))
-        if (ahead := p + ROT[r]) in O:
-            r = (r + 1) % 4
+        if (ahead := p + r) in O:
+            r *= 1j
         else:
             p = ahead
     return False
@@ -62,5 +58,5 @@ if __name__ == '__main__':
     freeze_support()
     pool = Pool()
     print(len(V := walk()))
-    task = partial(is_obstacle, start, P, ROT, O)
+    task = partial(is_obstacle, start, P, O)
     print(sum(1 for v in pool.map(task, V) if v))
